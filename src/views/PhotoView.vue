f354252539b530628611c14b30381368de4a779a
Tuija <til.utilities@proton.me>
2026-01-11 15:54:36 +0200

    Get single photo's data

--- src/views/PhotoView.vue
+++ src/views/PhotoView.vue
@@ -1,5 +1,34 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  let route = useRoute();
+  let photo = ref({});
+
+  onMounted(() => getPhotoByRouteParam());
+
+  function getPhotoByRouteParam() {
+
+    if (window.localStorage.getItem('photos') == null) {
+      return null;
+    }
+
+    if (route.params.id == null) {
+      return null;
+    }
+
+    let photos = JSON.parse(window.localStorage.getItem('photos'));
+    
+    let photoById = photos.find((photo) => photo.id === parseInt(route.params.id));
+    
+    if (photoById != undefined) {
+      photo.value = photoById;
+    }
+  };
+</script>
+
 <template>
   <hgroup>
-    <h1>Photo #{{ $route.params.id }}</h1>
+    <h1>Photo #{{ photo.id }}</h1>
   </hgroup>
 </template>
